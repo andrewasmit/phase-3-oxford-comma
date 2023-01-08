f3131115a5e070b2c0e73a295c1f96a25af6a620
andrew <andrewasmit@hotmail.com>
2023-01-07 20:22:36 -0600

    commit tree 9b47fa9bc9e9901c03a842a6d0008e5d640607c2

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+    case arr.length
+    when 1
+        arr.join('')
+    when 2
+        arr.join(' and ')
+    else 3
+        last = arr.pop
+        arr << "and " + last
+        arr.join(", ")
+    end
 end
